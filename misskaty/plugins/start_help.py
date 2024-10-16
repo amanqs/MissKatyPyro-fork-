61cefa9a358d7b9293678f390205729a5b74ce7c
amanqs <116822442+amanqs@users.noreply.github.com>
2024-10-16 10:20:03 +0700

    sdf

--- misskaty/plugins/start_help.py
+++ misskaty/plugins/start_help.py
@@ -25,7 +25,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(text="Commands ❓", callback_data="bot_commands"),
-            InlineKeyboardButton(text="Music Commands ❓", callback_data="settingsback_helper"),
+            InlineKeyboardButton(text="Music Commands ❓", callback_data="settings_back_helper"),
         ],
         [
             InlineKeyboardButton(
